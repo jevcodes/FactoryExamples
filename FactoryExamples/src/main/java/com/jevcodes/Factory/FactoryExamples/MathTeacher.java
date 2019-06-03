9c515093c5f9a1a99ce5e6e7525614346468920e
Jevic J <jevic.jardeleza>
2019-06-02 20:33:39 -0400

    initial commit

---
+++ FactoryExamples/src/main/java/com/jevcodes/Factory/FactoryExamples/MathTeacher.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.jevcodes.Factory.FactoryExamples;
+
+/**
+ * @author Jevic
+ *
+ */
+public class MathTeacher implements Teacher {
+
+	/* (non-Javadoc)
+	 * @see com.jevcodes.Factory.FactoryExamples.Teacher#teachTodaysLesson()
+	 */
+	public String teachTodaysLesson() {
+		return "Teach Algebra";
+	}
+
+}
---
+++ FactoryExamples/src/main/java/com/jevcodes/Factory/FactoryExamples/Teacher.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jevcodes.Factory.FactoryExamples;
+
+/**
+ * @author Jevic
+ *
+ */
+public interface Teacher {
+	
+	public String teachTodaysLesson();
+	
+}
